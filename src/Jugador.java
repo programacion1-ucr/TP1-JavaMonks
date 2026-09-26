50c8a86dfcf3243367bafab85c6ad3466ad71570
FelipedeMezervilleArceU <marcel.demezerville@ucr.ac.cr>
2026-09-26 00:48:55 -0600

    Iniciar ronda y Jugador
    Empece a hacer el metodo iniciar ronda y modifique un poco la clase jugador para que guarde como atributo la mano, sigue incompleto.

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,8 +1,14 @@
 public class Jugador{
-	private Jugador jugador1;
-	private Jugador jugador2;
 
-	public Jugador(Jugador jugador1, Jugador jugador2){
-		
+	private Ficha [] mano;
+
+	public Jugador(){
+		mano = new Ficha[] mano;
+	}
+	public void setMano(Ficha [] mano){
+		this.mano = mano;
+	}
+	public Ficha[] getMano(){
+		return mano;
 	}
 }
--- src/Partida.java
+++ src/Partida.java
@@ -9,9 +9,20 @@ public class Partida {
     private boolean partidaFinalizada;
 
 
-    public void iniciarRonda(){ //inicia la ronda, reparte las fichas a los jugadores y determina el primer turno
+   /* public void iniciarRonda(){ //inicia la ronda, reparte las fichas a los jugadores y determina el primer turno
+        mazo.barajar();
+        jugador1.setMano(mazo.manoInicial());
+        jugador2.setMano(mazo.manoInicial());
+        Ficha [] mano1 = jugador1.getMano();
+        Ficha [] mano2 = jugador2.getMano();
+        for (int i = 0; i < mano1.length; i++){
+            if (mano1.esDoble()){
+                //
+            }
+        nota: aun no esta terminado, no compila aun, apenas pueda lo termino.
+        }
 
-    }
+    }*/
     public void jugarTurno(){ //el jugador elige qué jugada hacer, se valida y se coloca, o se hace comer/pasar el turno
 
     }
